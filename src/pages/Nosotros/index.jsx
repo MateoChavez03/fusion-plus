c26409246e8860aae343ea492e8ba6ec4247de85
Eneas Baroni <eneasbaroni@gmail.com>
2022-08-31 23:57:17 -0300

    Inicio Nosotros

--- src/pages/Nosotros/index.jsx
+++ src/pages/Nosotros/index.jsx
@@ -3,9 +3,23 @@ import './index.css'
 
 const Nosotros = () => {
   return (
-    <div className='divContainer'>
+    <main className='divContainer row'>
+      <div className='homeBaner col-12 row'>
+        <div className='bannerText col-12 col-md-6 text-center'>
+          <p className='bannerP1'>Somos #fusionestudiomas</p>
+          <h1>ESTUDIO DEDICADO A SOCIAL MEDIA</h1>
+          <p className='bannerP2'>La esencia del Estudio es la de emprender 100%</p>
+          <p className='bannerP3'>En fusión + nos especializamos en creación y gestión de contenidos, manejo de redes sociales, creación y renovación de imagen e identidad, e-commerce, fotografía de productos, y <span>¡Mucho más!</span></p>
+          <div className='bannerBtns col-12 row'>
+            <button className='bannerBtn1 col-5'>CONTACTANOS</button>
+            <button className='bannerBtn2 col-5'>INSTAGRAM</button>
+          </div>
+        </div>      
+
+      </div>
+
       Nosotros
-    </div>
+    </main>
   )
 }
 
